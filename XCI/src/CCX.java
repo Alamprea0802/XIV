48bd147c9790fbafe6f77b0709fdbc1d210f48ad
Alamp <Alamp@DESKTOP-TVGQR6E.home>
2020-09-17 18:27:18 -0400

    A Little bit about me

---
+++ XCI/src/CCX.java
@@ -0,0 +1,33 @@
+
+public class CCX {
+	
+	public static void main (String [] args ) {
+		
+		System.out.print("hello there. My name is Andrew Lamprea. ");
+		
+		System.out.print("This is my Q&A assignment. ");
+		
+		System.out.print("I was once asked what I wanted to be when i grow up. ");
+		
+		System.out.print("The answer to that has always been clear to me. I want to be a robotics engineer, possibly within the airforce. ");
+		
+		System.out.print("I have also been asked if I was going to be the type of person to put morals in front of my own ambition. ");
+		
+		System.out.print("To this I have an answer I think everyone expected. Yes, yes I will, yet no at the same time. My ambition is to help people, and stay true to my idea of justice. Therefore morals, my morals, ARE my ambition. ");
+		
+		System.out.print("A lighter topic of interest of mine is the ocean. Ive always felt the ocean and the sky define our limmitations as a people, and that once we beat those, it is only a matter of time before every other barrier falls from our path as well");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+}
